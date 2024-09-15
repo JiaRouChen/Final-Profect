2d69f5e3b15be849466011fe07b64e4b737a9d4f
JiaRouChen <supple0414@gmail.com>
2024-09-15 22:12:32 +0800

    Add files via upload

---
+++ API/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+
+
+
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder
+            .WithOrigins("http://localhost:5230") // 允许的来源 URL
+            .AllowAnyMethod() // 允许所有 HTTP 方法
+            .AllowAnyHeader() // 允许所有头部
+    );
+});
+
+
+
+
+
+
+
+
+
+builder.Services.AddDbContext<dbProductsContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("dbProductsConnection")));
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseStaticFiles();
+app.UseCors("AllowSpecificOrigin");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
