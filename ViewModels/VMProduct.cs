be9f9c5a5cc034a3e5e8914af2a0be10b586a310
JiaRouChen <supple0414@gmail.com>
2024-09-15 21:36:21 +0800

    ViewModels

---
+++ ViewModels/VMProduct.cs
@@ -0,0 +1,11 @@
+﻿using D05Project.Models;
+
+namespace D05Project.ViewModels
+{
+    public class VMProduct
+    {
+  
+        public List<Product>? Product { get; set; }
+        public List<Category>? Category { get; set; }
+    }
+}
---
+++ ViewModels/VMStock.cs
@@ -0,0 +1,13 @@
+﻿using D05Project.Models;
+
+namespace D05Project.ViewModels
+{
+    public class VMStock
+    {
+  
+        public List<Product>? Product { get; set; }
+        public List<withdrawalRecord>? withdrawalRecord { get; set; }
+
+        public List<PurchaseRecord>? PurchaseRecord { get; set; }
+    }
+}
