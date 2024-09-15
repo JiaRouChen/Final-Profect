9d0be836835c2b1e4e4dcc4120da3e7e87e5b95b
JiaRouChen <supple0414@gmail.com>
2024-09-15 22:08:00 +0800

    Add files via upload

---
+++ API/Controllers/CategoriesController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductAPI.DTO;
+using ProductAPI.Models;
+
+namespace ProductAPI.Controllers
+{
+    [Route("apiCategories")]
+    [ApiController]
+    [EnableCors("AllowSpecificOrigin")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly dbProductsContext _context;
+
+        public CategoriesController(dbProductsContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/Categories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategory()
+        {
+
+
+            //var result =  _context.Category.Select(d => new CategoryDTO
+            //{
+
+            // CatId=d.CatId,
+            //CatName= d.CatName
+
+            //});
+
+            return await _context.Category.ToListAsync();
+        }
+
+        // GET: api/Categories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryDTO>> GetCategory(string id)
+        {
+            var category = await _context.Category.Select(d => new CategoryDTO
+            {
+
+                CatId = d.CatId,
+                CatName = d.CatName
+
+            }).Where(d =>d.CatId == id).SingleOrDefaultAsync();
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        // PUT: api/Categories/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(string id, [FromForm]Category category)
+        {
+            if (id != category.CatId)
+            {
+                return BadRequest();
+              
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Categories
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Category>> PostCategory([FromForm]Category category)
+        {
+            _context.Category.Add(category);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CategoryExists(category.CatId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetCategory", new { id = category.CatId }, category);
+        }
+
+        //DELETE: api/Categories/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            var category = await _context.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Category.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+
+
+        private bool CategoryExists(string id)
+        {
+            return _context.Category.Any(e => e.CatId == id);
+        }
+    }
+}
---
+++ API/Controllers/sProductsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+
+namespace ProductAPI.Controllers
+{
+    [Route("apiProducts")]
+    [ApiController]
+    public class sProductsController : ControllerBase
+    {
+        private readonly dbProductsContext _context;
+
+        public sProductsController(dbProductsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/sProducts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<sProduct>>> GetsProduct()
+        {
+            return await _context.sProduct.ToListAsync();
+        }
+
+        // GET: api/sProducts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<sProduct>> GetsProduct(string id)
+        {
+            var sProduct = await _context.sProduct.FindAsync(id);
+
+            if (sProduct == null)
+            {
+                return NotFound();
+            }
+
+            return sProduct;
+        }
+
+        // PUT: api/sProducts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutsProduct(string id, sProduct sProduct)
+        {
+            if (id != sProduct.PId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sProduct).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!sProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/sProducts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<sProduct>> PostsProduct(sProduct sProduct)
+        {
+            _context.sProduct.Add(sProduct);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (sProductExists(sProduct.PId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetsProduct", new { id = sProduct.PId }, sProduct);
+        }
+
+        // DELETE: api/sProducts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletesProduct(string id)
+        {
+            var sProduct = await _context.sProduct.FindAsync(id);
+            if (sProduct == null)
+            {
+                return NotFound();
+            }
+
+            _context.sProduct.Remove(sProduct);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool sProductExists(string id)
+        {
+            return _context.sProduct.Any(e => e.PId == id);
+        }
+    }
+}
